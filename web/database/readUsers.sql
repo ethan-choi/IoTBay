a21f0d8bf503761036396bf85e519ce746d1980c
eethanchoi <ethanchoi777@gmail.com>
2020-06-02 18:08:38 +1000

    Access User Management

--- web/database/readUsers.sql
+++ web/database/readUsers.sql
@@ -12,27 +12,27 @@
 
 SELECT * FROM IOTUSER.USERS;
 
-INSERT INTO USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME)
+INSERT INTO USERS ("NAME" , EMAIL, PASSWORD, "NUMBER", STATUS, "ROLE")
 VALUES
-('bob.smith@email.com', 'password123', 'Bob', 'Smith'),
-('jenny.tran@email.com', 'hidden123', 'Jenny', 'Tran'),
-('tom.pett@email.com', 'freefalling', 'Tom', 'Pett'),
-('billy.joe@email.com', 'firestarter', 'Billy', 'Joe'),
-('huey.lew@email.com', 'thenews123', 'Huey', 'Lew'),
-('bobby.dylan@email.com', 'rollingstone', 'Bobby', 'Dylan'),
-('george.marrison@email.com', 'sweetload', 'George', 'Marrison'),
-('jeff.flynn@email.com', 'bricks45', 'Jeff', 'Flynn'),
-('roy.orbby@email.com', 'prettywoe', 'Roy', 'Orbby'),
-('mick.astley@email.com', 'mickroll', 'Mick', 'Astley'),
-('bonnie.flyer@email.com', 'eclipse', 'Bonnie', 'Flyer'),
-('cindy.lauper@email.com', 'overtime24', 'Cindy', 'Lauper'),
+('Bob Smith', 'bob.smith@email.com', 'password123', '5021297705','Active', 'Staff'),
+('Jenny Tran', 'jenny.tran@email.com', 'hidden123', '1457120600', 'Active', 'Customer'),
+('Tom Pett', 'tom.pett@email.com', 'freefalling', '9316648451', 'Active', 'Staff'),
+('Billy Joe', 'billy.joe@email.com', 'firestarter', '5155323218', 'Active', 'Customer'),
+('Huey Lew', 'huey.lew@email.com', 'thenews123', '5293277886', 'Active', 'Staff'),
+('Bobby Dylan', 'bobby.dylan@email.com', 'rollingstone', '8376339990', 'Active','Staff'),
+('George Marrison', 'george.marrison@email.com', 'sweetload', '1936460951', 'Active','Customer'),
+('Jeff Flynn', 'jeff.flynn@email.com', 'bricks45', '9063295311', 'Active','Staff'),
+('Roy Orbby', 'roy.orbby@email.com', 'prettywoe', '4681988507', 'Active','Staff'),
+('Mick Astley', 'mick.astley@email.com', 'mickroll', '0489702116', 'Active','Customer'),
+('Bonnie Flyer', 'bonnie.flyer@email.com', 'eclipse', '0714563616', 'Active','Staff'),
+('Cindy Lauper', 'cindy.lauper@email.com', 'overtime24', '0946890886', 'Active','Customer'),
 
-('daryl.fall@email.com', 'outoftouch', 'Daryl', 'Fall'),
-('john.zoates@email.com', 'thelist', 'John', 'Zoates'),
-('david.bowel@email.com', 'heros89', 'David', 'Bowel'),
-('stevie.mickey@email.com', 'fireroom', 'Stevie', 'Mickey'),
-('kenny.rodgers@email.com', 'gambling', 'Kenny', 'Rodgers'),
-('nick.harrison@email.com', 'nextdoor', 'Nick', 'Harrison'),
+('Darly Fall', 'daryl.fall@email.com', 'outoftouch', '3981212419', 'Active','Customer'),
+('John Zoates', 'john.zoates@email.com', 'thelist', '7557100127','Active', 'Customer'),
+('David Bowel', 'david.bowel@email.com', 'heros89', '4253502341','Active', 'Staff'),
+('Stevie Mickey', 'stevie.mickey@email.com', 'fireroom', '1384624531','Active', 'Customer'),
+('Kenny Rodgers', 'kenny.rodgers@email.com', 'gambling', '3710411277','Active', 'Customer'),
+('Nick Harrison', 'nick.harrison@email.com', 'nextdoor', '6540567903','Active', 'Customer'),
 
-('paul.simo@email.com', 'callmeal', 'Paul', 'Simo'),
-('phil.pollins@email.com', 'genesis', 'Phil', 'Pollins')
+('Paul Simo', 'paul.simo@email.com', 'callmeal', '8973311470','Active', 'Customer'),
+('Phil Pollins', 'phil.pollins@email.com', 'genesis', '1703707579','Active', 'Customer')
