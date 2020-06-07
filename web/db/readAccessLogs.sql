95ec434f640b26d0711d500adb55ed42df4c95ef
eethanchoi <ethanchoi777@gmail.com>
2020-06-07 12:02:34 +1000

    Access log sample data

--- web/db/readAccessLogs.sql
+++ web/db/readAccessLogs.sql
@@ -23,4 +23,16 @@ VALUES
 ('2020-05-30', '18:02:10', 'Login', 'jeff.flynn@email.com' ),
 ('2020-05-30', '19:02:40', 'Logout', 'jeff.flynn@email.com' ),
 ('2020-06-01', '13:00:02', 'Login', 'jeff.flynn@email.com' ),
-('2020-06-01', '13:01:30', 'Logout', 'jeff.flynn@email.com' );
+('2020-06-01', '13:01:30', 'Logout', 'jeff.flynn@email.com' ),
+
+('2020-02-21', '03:33:33', 'Register', 'john.zoates@email.com' ),
+('2020-02-27', '10:02:10', 'Login', 'john.zoates@email.com' ),
+('2020-02-27', '11:30:40', 'Logout', 'john.zoates@email.com' ),
+('2020-06-01', '15:00:59', 'Login', 'john.zoates@email.com' ),
+('2020-06-01', '15:20:31', 'Logout', 'john.zoates@email.com' ),
+
+('2020-02-21', '12:02:11', 'Register', 'daryl.fall@email.com' ),
+('2020-02-27', '08:30:12', 'Login', 'daryl.fall@email.com' ),
+('2020-02-27', '08:59:12', 'Logout', 'daryl.fall@email.com' ),
+('2020-06-01', '03:00:59', 'Login', 'daryl.fall@email.com' ),
+('2020-06-01', '04:01:11', 'Logout', 'daryl.fall@email.com' );
