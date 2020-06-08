bd749fc9c97f1d22f003ae425359b251b72114c2
1186461130 <61954250+1186461130@users.noreply.github.com>
2020-06-09 01:11:16 +1000

    Add files via upload

---
+++ src/java/uts/isd/model/OrderDetail.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 1
+ */
+public class OrderDetail implements Serializable
+{
+    private int quantity;
+    private double totalPrice;
+
+    public OrderDetail(int quantity, double totalPrice) {
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    
+}
---
+++ src/java/uts/isd/model/OrderHistory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 1
+ */
+public class OrderHistory implements Serializable
+{
+    private int historyId;
+
+    public OrderHistory(int historyId) {
+        this.historyId = historyId;
+    }
+
+    public int getHistoryId() {
+        return historyId;
+    }
+
+    public void setHistoryId(int historyId) {
+        this.historyId = historyId;
+    }
+    
+    
+}
