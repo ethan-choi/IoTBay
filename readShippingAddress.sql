8472cb29a0eb3207580ab55d725ea695f18fab1b
1186461130 <61954250+1186461130@users.noreply.github.com>
2020-06-08 18:49:08 +1000

    Add files via upload

---
+++ readShippingAddress.sql
@@ -0,0 +1,28 @@
+SELECT * FROM IOTUSER.SHIPPING_ADDRESS;
+
+INSERT INTO SHIPPING_ADDRESS ("STREET_NUMBER", "STREET_NAME","SUBURB", "STATE","POSTCODE","EMAIL")
+VALUES
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com'),
+(1,'fairmpt street', 'lakmen','MSW',21312,'bob.smith@email.com')
+
+
+
